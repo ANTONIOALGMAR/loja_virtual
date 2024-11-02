7e4c4b54bb4309b1a92b8d1c00d419780bfe5af7
ANTONIOALGMAR <antonioalgmar@gmail.com>
2024-11-02 12:31:29 -0300

    refatoracao de usuarioRepository

--- src/main/java/com/antonio/lojavirtual/repository/UsuarioRepository.java
+++ src/main/java/com/antonio/lojavirtual/repository/UsuarioRepository.java
@@ -9,7 +9,7 @@ import com.antonio.lojavirtual.model.Usuario;
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
 
-	@Query(value = "select u from Usuario where u.login = ?1")
+	@Query(value = "select u from Usuario u where u.login = ?1")
 	Usuario findUserByLogin(String login );
 	
 }
